6eecee554c328c5e3c29e9ae7f429abd8014f332
Yura Husak <yuriy.husak@emilfreydigital.hr>
2024-07-21 13:44:19 +0300

    fix: remove commented code

--- src/tables/users/services/users.service.ts
+++ src/tables/users/services/users.service.ts
@@ -12,26 +12,3 @@ export class UserService extends CrudService<UserDto> {
     super(repo)
   }
 }
-/*
-@Injectable()
-export class UsersService {
-  constructor (@InjectRepository(UserDto) private readonly repo: Repository<UserDto>) { }
-
-  async getAll (): Promise<UserDto[] | null> {
-    return await this.repo.find()
-  }
-
-  async getById (id: string): Promise<UserDto | null> {
-    return await this.repo.findOne({ where: { id } })
-  }
-
-  async add (user: UserDto): Promise<UserDto | null> {
-    return await this.repo.save(this.repo.create(user))
-  }
-
-  async remove (id: string): Promise<UserDto | null> {
-    const found = await this.getById(id)
-    return this.repo.remove(found)
-  }
-}
- */
